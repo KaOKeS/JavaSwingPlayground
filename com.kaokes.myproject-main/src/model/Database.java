75c6709606ba3b9172127afdf29f36fa2f850961
Dawid Pater <kaokes@gmail.com>
2023-03-26 22:41:09 +0200

    SQL queries added.

--- com.kaokes.myproject-main/src/model/Database.java
+++ com.kaokes.myproject-main/src/model/Database.java
@@ -1,26 +1,27 @@
 package model;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Database {
+    private static final String CHECK_SQL = "SELECT COUNT(*) AS COUNT FROM PEOPLE WHERE ID=?";
+    private static final String INSERT_SQL = "INSERT INTO PEOPLE (ID,NAME,AGE,EMPLOYMENT_STATUS,TAX_ID,US_CITIZEN,GENDER,OCCUPATION) VALUES (?,?,?,?,?,?,?,?)";
+    private static final String UPDATE_SQL = "UPDATE PEOPLE SET NAME=?,AGE=?,EMPLOYMENT_STATUS=?,TAX_ID=?,US_CITIZEN=?,GENDER=?,OCCUPATION=? WHERE ID=?";
     private final List<Person> people = new LinkedList<>();
     private Connection con;
 
-    public void connect() {
+    public void connect() throws Exception{
         if (con != null) {
             return;
         }
         try {
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/swingtest", "root", "admin");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new Exception("Can not connect to database");
         }
     }
 
@@ -32,6 +33,61 @@ public class Database {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void save() throws SQLException {
+        PreparedStatement checkStatement = con.prepareStatement(CHECK_SQL);
+
+        for (Person person : people) {
+            Long id = person.getId();
+            String name = person.getName();
+            String occupation = person.getOccupation();
+            EmploymentCategory empCat = person.getEmpCat();
+            AgeCategory ageCategory = person.getAgeCategory();
+            String taxId = person.getTaxId();
+            boolean usCitizen = person.isUsCitizen();
+            Gender gender = person.getGender();
+
+            checkStatement.setLong(1, id);
+            ResultSet resultSet = checkStatement.executeQuery();
+            resultSet.next();
+            long count = resultSet.getLong(1);
+
+            if (count==0){
+                System.out.println("Inserting person with id: " + id);
+
+                PreparedStatement insertStatement = con.prepareStatement(INSERT_SQL);
+                insertStatement.setLong(1,id);
+                insertStatement.setString(2,name);
+                insertStatement.setString(3,ageCategory.name());
+                insertStatement.setString(4,empCat.name());
+                insertStatement.setString(5,taxId);
+                insertStatement.setBoolean(6,usCitizen);
+                insertStatement.setString(7,gender.name());
+                insertStatement.setString(8,occupation);
+
+                insertStatement.executeUpdate();
+                insertStatement.close();
+            }
+            else {
+                System.out.println("Updating person with id: " + id);
+
+                PreparedStatement updateStatement = con.prepareStatement(UPDATE_SQL);
+                int col=1;
+                updateStatement.setString(col++,name);
+                updateStatement.setString(col++,ageCategory.name());
+                updateStatement.setString(col++,empCat.name());
+                updateStatement.setString(col++,taxId);
+                updateStatement.setBoolean(col++,usCitizen);
+                updateStatement.setString(col++,gender.name());
+                updateStatement.setString(col++,occupation);
+                updateStatement.setLong(col,id);
+
+                updateStatement.executeUpdate();
+                updateStatement.close();
+            }
+        }
+        checkStatement.close();
     }
 
     public void addPerson(Person person) {
